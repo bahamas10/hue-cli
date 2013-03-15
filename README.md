4080d5051459f8bf0e59aaa9f69f6ce273a01ab0
Dave Eddy <dave@daveeddy.com>
2013-03-15 01:26:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,202 @@
+hue(1)
+======
+
+A command line interface to [phillips hue](http://meethue.com)
+
+Installation
+------------
+
+First, install [Node.js](http://nodejs.org), then:
+
+    npm install -g hue-cli
+
+...and the executable will be installed globally as `hue`
+
+Example
+-------
+
+### starting off
+
+First, let's search for nearby base stations
+
+    $ hue search
+    1 stations found
+
+    1: 10.0.1.218
+
+Pass in `-j` for json if you'd like
+
+    $ hue -j search
+    [
+      "10.0.1.218"
+    ]
+
+Next, let's try to list the lights on that base station
+
+    $ hue -H 10.0.1.218 lights
+    error: application not registered, run `hue register` first
+
+Oops, this app isn't registered yet, let's go ahead and do that
+
+    $ hue -H 10.0.1.218 register
+    please go and press the link button on your base station
+    Hue Base Station paired!
+
+### listing lights
+
+All you had to do was press the button on your base station to register, cool
+right?  Let's re run the lights command
+
+    $ hue -H 10.0.1.218 lights
+    1: Mike 1
+    2: Mike 2
+    3: Dave closet
+    4: Hallway 2
+    5: Hallway 1
+    6: Front hallway
+    7: Dave Ledge Left
+    8: Dave Ledge Right
+
+Again, `-j` if you'd like json output.
+
+Running with the command `lights` will give us a list of all the lights
+connected to the base station.
+
+Before we continue, let's create a configuration file.  In the file we can
+set the default host to connect to, so we don't have to keep supplying the
+`-H` argument.  Run:
+
+    $ hue --init
+    config file written to `~/.hue.json`
+
+Now, modify that file and replace `null` with `10.0.1.218`, or whatever
+your IP or hostname is. Now we'll no longer have to supply the `-H` argument
+with every command.
+
+From here, we can get information about a single light like:
+
+    $ hue lights 1
+    {
+      "state": {
+        "on": true,
+        "bri": 141,
+        "hue": 13122,
+        "sat": 211,
+        "xy": [
+          0.5119,
+          0.4147
+        ],
+        "ct": 467,
+        "alert": "none",
+        "effect": "none",
+        "colormode": "ct",
+        "reachable": true
+      },
+      "type": "Extended color light",
+      ...
+    }
+
+### controlling the lights
+
+Let's actually mess with the lights now.  Let's turn on the light in my closet.
+
+    $ hue lights 3 on
+    light 3 success
+
+You'll have to trust me on this, but the light in my closet just turned on.  What
+about both lights in the hallway?
+
+    $ hue lights 4,5 on
+    light 4 success
+    light 5 success
+
+And both of the lights in the hallway just turned on.  What if we try to turn on a non-existent light?
+
+    $ hue lights 99 on
+    light 99 failed: resource, /lights/99/state, not available
+
+Cool, errors handled properly.  Let's see some more examples
+
+    $ hue lights off
+    light 1 success
+    light 2 success
+    light 3 success
+    ...
+
+This is shorthand for
+
+    $ hue lights all off
+
+Where `all` is a recognized keyword for all lights in the system.  You can also:
+
+    $ hue lights off
+
+To quickly turn off all lights on the system
+
+### controlling colors
+
+> We can turn the lights on and off, that's great... what about colors?
+
+You want colors? how about hex
+
+    $ hue lights 4 ffffff
+    light 4 success
+
+We just set the light in the hallway to pure white, hex `ffffff`.  Let's go crazy
+and turn all of the lights in the house red (this is where we need the `all` keyword)
+
+    $ hue lights all ff0000
+    light 1 success
+    light 2 success
+    ...
+
+It's worth noting here that, because this tool is written in Node, all requests to the
+lights are done concurrently.  This means we don't have to wait for light 1 to finish
+before we instruct light 2 to change, nor wait for light 2 to finish before we instruct
+light 3 to change, and so on.
+
+Shorthand hex is also cool
+
+    $ hue lights 3,4 0f0
+    light 3 success
+    light 4 success
+
+Now lights 3 and 4 are green
+
+Usage
+-----
+
+    Usage: hue [-H host] [--json] [command]
+
+    control phillips hue over the command line
+
+    examples
+      hue config          # view the hue config
+      hue lights          # get a list of lights
+      hue lights 5        # get information about light 5
+      hue lights 5,6,7 on # turn lights 5 6 and 7 on
+      hue help            # this message
+      hue register        # register this app to hue, done automatically
+      hue search          # search for hue base stations
+
+    commands
+      config, lights, help, register, search
+
+    options
+      -h, --help     print this message and exit
+      -H, --host     the hostname or ip of the bastion to control
+      -i, --init     initialize the config file at /Users/dave/.hue.json
+      -j, --json     force output to be in json
+      -u, --updates  check for available updates
+      -v, --version  print the version number and exit
+
+Credits
+-------
+
+* [Phillips hue](http://meethue.com): I assume you know what this is by now
+* [hue.js](https://github.com/thatguydan/hue.js): Node.js hue client
+
+License
+-------
+
+MIT
---
+++ hue-cli.js
@@ -0,0 +1,228 @@
+#!/usr/bin/env node
+/**
+ * Hue Command Line Interface
+ *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ * Date: 3/14/13
+ * License: MIT
+ */
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+var getopt = require('posix-getopt');
+var Hue = require('hue.js');
+
+var package = require('./package.json');
+
+var app = 'node-hue-cli';
+var configfile = path.join(process.env.HOME, '.hue.json');
+var config;
+try {
+  config = require(configfile);
+} catch (e) {
+  config = {};
+}
+
+/**
+ * return the usage statement
+ */
+function usage() {
+  return [
+    'Usage: hue [-H host] [--json] [command]',
+    '',
+    'control phillips hue over the command line',
+    '',
+    'examples',
+    '  hue config          # view the hue config',
+    '  hue lights          # get a list of lights',
+    '  hue lights 5        # get information about light 5',
+    '  hue lights 5,6,7 on # turn lights 5 6 and 7 on',
+    '  hue help            # this message',
+    '  hue register        # register this app to hue, done automatically',
+    '  hue search          # search for hue base stations',
+    '',
+    'commands',
+    '  config, lights, help, register, search',
+    '',
+    'options',
+    '  -h, --help     print this message and exit',
+    '  -H, --host     the hostname or ip of the bastion to control',
+    '  -i, --init     initialize the config file at ' + configfile,
+    '  -j, --json     force output to be in json',
+    '  -u, --updates  check for available updates',
+    '  -v, --version  print the version number and exit'
+  ].join('\n');
+}
+
+// command line arguments
+var options = [
+  'h(help)',
+  'H:(host)',
+  'i(init)',
+  'j(json)',
+  'u(updates)',
+  'v(version)'
+].join('');
+var parser = new getopt.BasicParser(options, process.argv);
+
+var option;
+var json = false;
+var huehost = config.host;
+while ((option = parser.getopt()) !== undefined) {
+  switch (option.option) {
+    case 'h': console.log(usage()); process.exit(0);
+    case 'H': huehost = option.optarg; break;
+    case 'i':
+      fs.writeFileSync(configfile, JSON.stringify(config, null, 2));
+      console.log('config file written to `%s`', configfile);
+      process.exit(0);
+    case 'j': json = true; break;
+    case 'u': // check for updates
+      require('latest').checkupdate(package, function(ret, msg) {
+        console.log(msg);
+        process.exit(ret);
+      });
+      return;
+    case 'v': console.log(package.version); process.exit(0);
+    default: console.error(usage()); process.exit(1); break;
+  }
+}
+var args = process.argv.slice(parser.optind());
+
+// command switch
+var client, lights;
+switch (args[0]) {
+  case 'config': // get the config as json
+    client = getclient();
+    client.config(function(err, data) {
+      console.log(JSON.stringify(err || data, null, 2));
+    });
+    break;
+  case 'help': // print the help message
+    console.log(usage());
+    break;
+  case 'lights': // mess with the lights
+    client = getclient();
+    getlights(client, function(lights) {
+      // if there are no lights specified, return the list of lights
+      var keys = Object.keys(lights);
+      if (!args[1]) {
+        if (json) return console.log(JSON.stringify(lights, null, 2));
+        keys.forEach(function(key) {
+          console.log('%s: %s', key, lights[key].name);
+        });
+        return;
+      }
+
+      // handle shortucts like `lights off`, `lights all on`
+      var l = args[1].split(',');
+      switch (l[0]) {
+        case 'all': l = keys; break;
+        case 'on': l = keys; args[2] = 'on'; break;
+        case 'off': l = keys; args[2] = 'off'; break;
+      }
+      // if there is no action specified, return info about the first light given
+      if (!args[2]) {
+        client.light(l[0], function(err, data) {
+          console.log(JSON.stringify(err || data, null, 2));
+        });
+        return;
+      }
+
+      switch (args[2]) {
+        case 'off': l.forEach(function(id) { client.off(id, callback(id)); }); break;
+        case 'on': l.forEach(function(id) { client.on(id, callback(id)); }); break;
+        default: // assume hex values
+          var hex = args[2];
+          var rgb = hex2rgb(hex);
+
+          l.forEach(function(id) {
+            client.rgb(id, rgb[0], rgb[1], rgb[2], callback(id));
+          });
+          break;
+      }
+
+      function callback(id) {
+        return function(err, data) {
+          if (json) return console.log(JSON.stringify(err || data, null, 2));
+          if (err) return console.error('light %d failed: %s', id, err.description);
+          console.log('light %d success', id);
+        }
+      }
+    });
+    break;
+  case 'register': // register this app
+    client = getclient();
+    console.log('please go and press the link button on your base station');
+    client.register(function(err) {
+      if (err) return cb(err);
+      console.log('Hue Base Station paired!')
+    });
+    break;
+  case 'search': // search for base stations
+    Hue.discover(function(stations) {
+      if (json) return console.log(JSON.stringify(stations, null, 2));
+      console.log('%d stations found\n', stations.length);
+      stations.forEach(function(name, i) { console.log('%d: %s', i+1, name); });
+    });
+    break;
+  default: // uh oh
+    console.error('unknown command');
+    process.exit(1);
+}
+
+// wrapper around get client to error on failure
+function getclient() {
+  if (!huehost) {
+    console.error([
+      'error: host not set',
+      '',
+      'search for hosts with: hue search',
+      'then run with `-H <host>` or create a config file with `--init`',
+    ].join('\n'));
+    process.exit(1);
+  }
+
+  // create the client
+  var client = Hue.createClient({
+    stationIp: huehost,
+    appName: app
+  });
+  return client;
+}
+
+// wrapper around get lights to error on failure
+function getlights(client, cb) {
+  // checking for lights will also help us ensure the app is registered
+  // from exmample here https://github.com/thatguydan/hue.js
+  client.lights(function(err, lights) {
+    if (err && err.type === 1) {
+      console.error('error: application not registered, run `hue register` first');
+      process.exit(1);
+    }
+    if (err) throw err;
+    cb(lights)
+  });
+}
+
+// convert a 3 or 6 character hex string to rgb
+function hex2rgb(hex) {
+  var r, g, b;
+
+  if (hex.length === 3) {
+    r = todec(hex[0], hex[0]);
+    g = todec(hex[1], hex[1]);
+    b = todec(hex[2], hex[2]);
+  } else {
+    r = todec(hex[0], hex[1]);
+    g = todec(hex[2], hex[3]);
+    b = todec(hex[4], hex[5]);
+  }
+
+  return [r, g, b];
+
+  function todec(h, i) {
+    return parseInt(h + '' + i, 16)
+  }
+}
